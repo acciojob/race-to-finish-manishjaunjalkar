9550796086b4bcc40e3fe83386f1b1cd642f226f
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-18 01:28:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
 window.promises = [];
 
-
-
 // Do not change the code above this
 // add your promises to the array `promises`
-let x = Math.floor(Math.random() * 5) + 1;
-console.log(x);
-let promise1 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("1");
-	}, x * 1000)
-});
 
-let promise2 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("2");
-	}, x * 1000)
-})
-	let promise3 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("3");
-	}, x * 1000)
-})
-	let promise4 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("4");
-	}, x * 1000)
-})
-	let promise5 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("5");
-	}, x * 1000)
-})
+let opt = document.getElementById("output");
 
-	promises.push(promise1);
-	promises.push(promise2);
-	promises.push(promise3);
-	promises.push(promise4);
-	promises.push(promise5);
+window.promises = [new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),
+				  new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),
+				  new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),
+				  new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),
+				  new Promise((resolve)=>{resolve(Math.floor(Math.random() * (5 - 1) + 1))}),];
 
-	Promise.any(promises).then((value) => {
-    document.getElementById("output").innerText = value;
 
-// Do not change the code above this
-// add your promises to the array `promises`
+Promise.any(window.promises).then((data)=>{
+	let text = document.createTextNode(data);
+	opt.append(text);
+})
