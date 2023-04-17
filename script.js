6d50cf18de66cc609f8113752787c1531732ed48
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-04-18 00:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.promises = [];
+var output = document.getElementById("output");
+var promise = new Promise((resolve)=>{
+	setTimeout(()=>{
+		output.innerHTML="ans1"
+	resolve();
+	},Math.floor(Math.random() * 5000))
+});
+
+Promise.any();
 
 // Do not change the code above this
 // add your promises to the array `promises`
